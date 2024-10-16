8f0b6087d216e72e074b0cda9771e8707a708df4
Dishant Goyal <dishant.in@gmail.com>
2024-10-17 04:33:36 +0530

    a simple example of makeFile

---
+++ example_makefile/class.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "class.h"
+
+using namespace std;
+
+foreign::foreign() {
+	std::cout<<"entered a foreign class! "<<std::endl;
+}
---
+++ example_makefile/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "class.h"    // user defined class kept in quotes instead of <>
+
+using namespace std;
+
+int main() {
+	std::cout<<"hello main!"<<std::endl;
+	foreign obj;
+return 0;
+}
